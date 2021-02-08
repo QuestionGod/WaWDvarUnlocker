4c9e4f8bbca13d67154b0b3e0e7ace01885c1de5
QuestionGod <69332340+QuestionGod@users.noreply.github.com>
2021-02-08 09:31:25 -0600

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,3 +1,8 @@
+// WaW Dvar Unlocker For Multiplayer mode and Zombies Mode. 
+// Made this in what would have been 2 mins but i guess i never took note of the addresses for the two functions... so 5 mins for some random YouTube video showing how to get access to CG_Fov in ZM.
+// Why? Because i have always wanted to make a GitHub post, however shitty it may be... but code is 100% mine; all 40 lines of it xd
+// 2 seperate DLLs have been compiled and are ready for immediate use, inject with any injector of choice
+// Maybe i post more WaWmp cheats here in future, idk. Game lacks any REAL shit other than chams and some shat distance targeting aimbots :P
 
 // Hook Addresses
 DWORD dwDvarUnlockerMP = 0x0;
